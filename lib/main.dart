024efde8d28d3a110edad5e27ba2f2f06b9a2caf
chet.ram <chet.ram@netscapelabs.com>
2020-07-27 17:23:55 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        fontFamily: 'regular',
+        fontFamily: 'regular',  
         primaryColor: CColor.PRIMARYCOLOR,
 
         accentColor:CColor.PRIMARYCOLOR,
